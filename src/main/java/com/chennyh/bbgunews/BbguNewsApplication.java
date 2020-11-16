4b9b6b6956d937386a9a434bf418c9867ecaacd2
Chennyh <chennyh@qq.com>
2020-11-16 17:01:39 +0800

    project init

---
+++ src/main/java/com/chennyh/bbgunews/BbguNewsApplication.java
@@ -0,0 +1,13 @@
+package com.chennyh.bbgunews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbguNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbguNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chennyh/bbgunews/BbguNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chennyh.bbgunews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbguNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
