9c08450a2d537d450e5a50ae70dfe131c471deef
Chennyh <chennyh@qq.com>
2020-12-28 13:05:36 +0800

    适配Spring boot 2.4.0,修改跨域实现方法

--- src/main/java/com/chennyh/bbgunews/config/GlobalCorsConfig.java
+++ src/main/java/com/chennyh/bbgunews/config/GlobalCorsConfig.java
@@ -1,10 +1,8 @@
 package com.chennyh.bbgunews.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Chennyh
@@ -12,24 +10,20 @@ import org.springframework.web.filter.CorsFilter;
  * @description 全局跨域配置
  */
 @Configuration
-public class GlobalCorsConfig {
+public class GlobalCorsConfig implements WebMvcConfigurer {
     /**
      * 允许跨域调用的过滤器
      */
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        //允许所有域名进行跨域调用
-//        config.addAllowedOrigin("*");
-        config.addAllowedOrigin("http://localhost:8090");
-        //允许跨越发送cookie
-        config.setAllowCredentials(true);
-        //放行全部原始头信息
-        config.addAllowedHeader("*");
-        //允许所有请求方法跨域调用
-        config.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //允许所有域名进行跨域调用
+                .allowedOriginPatterns("*")
+                //允许跨越发送cookie
+                .allowCredentials(true)
+                //放行全部原始头信息
+                .allowedHeaders("*")
+                //允许所有请求方法跨域调用
+                .allowedMethods("*");
     }
 }
